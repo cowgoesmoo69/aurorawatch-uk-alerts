6c64f730ee0cea1bd57028bf809238517529eb48
cowgoesmoo69 <earp.tom@gmail.com>
2026-01-24 17:50:52 +0000

    Add threshold functionality

--- app/aurorawatch-uk-alerts.py
+++ app/aurorawatch-uk-alerts.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 
 import argparse
-from pushover import send_alert
-from datetime import date, datetime, timedelta
-from lxml import etree
 import os
 import random
-import requests
 import time
+from aurorawatchuk import get_status
+from datetime import date, datetime, timedelta
+from pushover import send_alert
 
 SCRIPT_VERSION="2.0.0"
-
-AWUK_URL = "https://aurorawatch-api.lancs.ac.uk/0.2.5/status/all-site-status.xml"
 
 def argparser():
     parser = argparse.ArgumentParser(
@@ -26,6 +23,12 @@ def argparser():
         help="Turns on DEBUG output",
         action="store_true",
     )
+    parser.add_argument(
+        "-t",
+        "--ttl",
+        help="Sets a custom ttl in seconds. Default is four hours.",
+        default=14400,
+        )
     parser.add_argument("-v", "--version", action="version", version=SCRIPT_VERSION)
     return parser.parse_args()
 
@@ -38,7 +41,7 @@ def pre_checks():
     args = argparser()
     global DEBUG
     DEBUG = args.debug
-    # Validate supplied threshold value.
+    # Validate threshold value.
     if DEBUG:
         print("Validating supplied threshold value.")
     global THRESHOLD
@@ -52,6 +55,20 @@ def pre_checks():
         raise TypeError("Threshold must be an integer.")
     if DEBUG:
         print("Threshold passed validation.")
+    # Validate ttl value.
+    global TTL
+    TTL = args.ttl
+    if DEBUG:
+        print("Validating TTL.")
+    if isinstance(TTL, int):
+        if TTL > 0 and TTL <= 31536000:
+            pass
+        else:
+            raise ValueError("TTL must be betwen 1 and 31536000.")
+    else:
+        raise TypeError("TTL must be an integer.")
+    if DEBUG:
+        print("TTL passed validation.")
     # Check for necessary environment variables.
     if DEBUG:
         print("DEBUG: Checking for environment variables.")
@@ -65,79 +82,12 @@ def pre_checks():
         print("DEBUG: Environment variables found.")
 
 
-def get_status_ids():
-    # Retrieves the all-site-status.xml file from AWUK and returns all status_id values.
-    try:
-        # Fetch xml file.
-        if DEBUG:
-            print("DEBUG: Fetching all-site-status.xml from AWUK.")
-        response = requests.get(
-            AWUK_URL,
-            headers={
-                "referer": "https://github.com/cowgoesmoo69/aurorawatch-uk-alerts"
-            },
-            timeout=10,
-        )
-        if DEBUG:
-            print("DEBUG: Checking if fetch was successful.")
-        response.raise_for_status()
-        if DEBUG:
-            print("DEBUG: Fetch was successful.")
-        # Process xml file for status IDs.
-        if DEBUG:
-            print("DEBUG: Processing all-site-status.xml.")
-        root = etree.fromstring(response.content)
-        sites = root.xpath("//site_status")
-        status_ids = []
-        for site in sites:
-            status_ids.append(
-                {
-                    # "site_id": site.get("site_id"), # Might use in the future.
-                    # "site_url": site.get("site_url"), # Might use in the future.
-                    "status_id": site.get("status_id"),
-                }
-            )
-        if DEBUG:
-            print("DEBUG: Returning status IDs.")
-        return status_ids
-    except Exception as e:
-        print(f"An exception occurred while retrieving/processing site statuses: {e}")
-
-
-def get_current_status(
-    status_ids,
-):
-    # Determine the lowest-ranked status ID across all sites and return it.
-    #
-    # Function only returns red if all sites are reporting red. Even if all sites are
-    # reporting red, there is no guarantee you will see aurora borealis in the UK;
-    # variables such as cloud cover, weather, light pollution etc. may prevent
-    # aurora being visible in your location.
-    if DEBUG:
-        print("DEBUG: Checking for lowest-ranked status ID.")
-    RANK_ORDER = ["green", "yellow", "amber", "red"]
-    statuses = {s["status_id"] for s in status_ids}
-    for rank in RANK_ORDER:
-        if rank in statuses:
-            if DEBUG:
-                print(f"DEBUG: Lowest-ranked status ID is {rank}, returning.")
-            return rank
-    if DEBUG:
-        print("DEBUG: Failed to find a valid status ID, returning None.")
-    return None
-
-
 def go_to_sleep():
     # Calculate time of next check. Top of next hour, plus a random number of minutes
-    # and seconds so that check is performed in first ten minutes of hour. Avoids hitting
-    # the AWUK API at exactly the same time each hour.
-    #
-    # Aurorawatch UK ask that individual clients leave at least three minutes between
-    # requests. However, since aurora borealis is not an on/off phenomenon, hourly
-    # checks are plenty fast enough.
+    # and seconds so that check is performed in first five minutes of hour.
     t = datetime.now()
     n = t.replace(minute=0, second=0, microsecond=0) + timedelta(
-        hours=1, minutes=random.randint(0, 9), seconds=random.randint(0, 59)
+        hours=1, minutes=random.randint(0, 4), seconds=random.randint(0, 59)
     )
     if DEBUG:
         print(f"DEBUG: Next check at {n.strftime('%Y-%m-%d %H:%M:%S')}")
@@ -151,17 +101,17 @@ def go_to_sleep():
 
 def main():
     while True:
-        status_ids = get_status_ids()
-        status = get_current_status(status_ids)
-        # Send alert only if all sites reporting red.
-        if status == "red":
+        status = get_status
+        if status >= THRESHOLD:
+            STATUS_TEXT = ["GREEN", "YELLOW", "AMBER", "RED"]
             args = {
                 "token": PUSHOVER_APP_TOKEN,
                 "user": PUSHOVER_USER_KEY,
-                "message": "All sites reporting red status.",
-                "priority": 1,
-                "ttl": (3600 * 4)
+                "message": f"AuroraWatch UK Status: {STATUS_TEXT[status]}.",
+                "ttl": TTL
                 }
+            if status == 3:
+                args["priority"] = 1
             send_alert(**args)
         go_to_sleep()
 
